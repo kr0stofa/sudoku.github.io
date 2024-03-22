c89505a05b7b31bf4e459b64bf25c9a1b5e65de1
David Goh <35485082+kr0stofa@users.noreply.github.com>
2024-03-22 11:41:19 +0800

    ADD: mobile version

---
+++ precache-manifest.31c64d9c5bb4cd034c2141f170f1de1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce1a938d2e185cd08931bd7fee850bc",
+    "url": "/sudoku.github.io/index.html"
+  },
+  {
+    "revision": "5a5c62dd5b53b19a12f5",
+    "url": "/sudoku.github.io/static/css/main.c830b30b.chunk.css"
+  },
+  {
+    "revision": "eea811b2d33e96306f07",
+    "url": "/sudoku.github.io/static/js/2.4d800d35.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/sudoku.github.io/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "5a5c62dd5b53b19a12f5",
+    "url": "/sudoku.github.io/static/js/main.0df52693.chunk.js"
+  },
+  {
+    "revision": "87186605cbaeeeff89e0",
+    "url": "/sudoku.github.io/static/js/runtime~main.36cfb978.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/sudoku.github.io/static/media/logo.06e73328.svg"
+  }
+]);
